fe9eeeba3ee65cb1cf084feaa6e8cfa4ff7dced2
Joe Villegas <joexvillegas@gmail.com>
2021-01-15 20:42:01 -0800

    Corrected Web Build asset locations

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,23 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "8c78e24a299034a731a363ca29bc08a7",
+  "assets/AssetManifest.json": "470e1eff69dc3516fce60366d877aa93",
+"assets/assets/audio/soundboard/air_horn.mp3": "28f8817f0e7a8634bb2164ceaaa6b2ee",
+"assets/assets/audio/soundboard/aol_you_got_mail.wav": "7a19b2c32e89a9bdcb694082234e461c",
+"assets/assets/audio/soundboard/croissant_sound.mp3": "4a413c8f20ecb45b882daf4026f03b0f",
+"assets/assets/audio/soundboard/damn_son.mp3": "a6b3b134de37ec2f55e934816916fbe9",
+"assets/assets/audio/soundboard/DISTORTED_Bass_Boost.mp3": "6465a8a4fbc7f0d1208065284d92d1de",
+"assets/assets/audio/soundboard/HITMARKER.mp3": "7e1dd407d35b49abeddbd2ca0577a119",
+"assets/assets/audio/soundboard/lets_fucking_goooo.mp3": "1cf8488e06c50e37b3ad806beb05c29e",
+"assets/assets/audio/soundboard/Nate_Dog_Hold_Up.mp3": "759627607ac26ed1e43934921573b283",
+"assets/assets/audio/soundboard/Smoke_Weed_Everyday.mp3": "c0f8ddc78cfe36a948e08fd818fd6392",
+"assets/assets/audio/soundboard/Unreal_Tournament%2520_Holy_Shit.wav": "222ebe50ab253fbd2402e94dad6f916d",
+"assets/assets/audio/soundboard/Unreal_Tournament_Monster_Kill.mp3": "93df556b4533103c4909c6fb988cba7f",
+"assets/assets/audio/soundboard/whats_gucci_gorf.mp3": "0ce330df22c00696823d537b03c8441f",
+"assets/assets/audio/soundboard/windows_critical_stop.mp3": "53d87475403c9a34131d5e7420920bcc",
 "assets/assets/fonts/MyFlutterApp.ttf": "500762435d6de2b8b75c8b6c5a3d6595",
 "assets/assets/icons/sounds/letsfuckinggo.svg": "861dee5dee6a28cd7ee07d6093165e46",
-"assets/assets/references/soundlist.json": "f0cb76b8c116f1c65250475118f33528",
+"assets/assets/references/soundlist.json": "b0838d9a4a84e80c48d9f100b73b123d",
 "assets/FontManifest.json": "1e2b2284e66a9e54db0bfe7ac4f8d5d6",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/NOTICES": "2a2f4cb59eee90ab47ca07d8e762e26f",
@@ -14,9 +27,9 @@ const RESOURCES = {
 "favicon.png": "16daa1e75c75abade096971827289f33",
 "icons/Icon-192.png": "698704cb92d9a515459cc6ee65d70342",
 "icons/Icon-512.png": "05c3c10cf7bb027464fae82f6ba90f12",
-"index.html": "5db6cbfd24c0e56a7447c1a5a63625c2",
-"/": "5db6cbfd24c0e56a7447c1a5a63625c2",
-"main.dart.js": "b70417a898586cd00458b8d359acc7f7",
+"index.html": "04c60b89872c10b969606d00c51d620e",
+"/": "04c60b89872c10b969606d00c51d620e",
+"main.dart.js": "887215573a3d5298bfce651bb026a8d4",
 "manifest.json": "57e2b04b2a9b1384bc2c10657378f0e2",
 "version.json": "99d5daa2da2f7477513c22d4eb9287e9"
 };
