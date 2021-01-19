d6d205620fe74ca93525ca5aab012cbc66fa7847
Joe Villegas <joexvillegas@gmail.com>
2021-01-18 22:32:36 -0800

    Update icons and add sound

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "741b3e7e63430ae768d6b42c1d670a5c",
+  "assets/AssetManifest.json": "a7468e9c74d2fe3f1cc2918e948f4d66",
 "assets/assets/audio/soundboard/air_horn.mp3": "28f8817f0e7a8634bb2164ceaaa6b2ee",
 "assets/assets/audio/soundboard/aol_you_got_mail.wav": "7a19b2c32e89a9bdcb694082234e461c",
 "assets/assets/audio/soundboard/Baku_Got_DAMN.mp3": "3338bebb7e4fad0b7f6b4b5efe2d4257",
@@ -19,8 +19,20 @@ const RESOURCES = {
 "assets/assets/audio/soundboard/whats_gucci_gorf.mp3": "0ce330df22c00696823d537b03c8441f",
 "assets/assets/audio/soundboard/windows_critical_stop.mp3": "53d87475403c9a34131d5e7420920bcc",
 "assets/assets/fonts/MyFlutterApp.ttf": "500762435d6de2b8b75c8b6c5a3d6595",
+"assets/assets/icons/sounds/baku.jpg": "a98360dfd2a68845e92d3d5cd4c50d84",
+"assets/assets/icons/sounds/bass.jpg": "0fbc0816b537b003d703d82a9a00498c",
+"assets/assets/icons/sounds/carl_wheezer.jpg": "3f76ec909bd7feaca086867a9f4fee7e",
+"assets/assets/icons/sounds/garfielf.jpg": "97627f50b8f14ce9d94aa6be23111ae5",
+"assets/assets/icons/sounds/letsfuckinggo.png": "9a84b5058d85544e75be6b68b15f9196",
 "assets/assets/icons/sounds/letsfuckinggo.svg": "861dee5dee6a28cd7ee07d6093165e46",
-"assets/assets/references/soundlist.json": "b3cf72877fc5c9f05f4ccceaeea7ccd9",
+"assets/assets/icons/sounds/mlg.webp": "da5a98d69f7d8224378a170ffe5cec70",
+"assets/assets/icons/sounds/nate_dog.jpg": "d6910a876fd4bc7e7183ea92f705a614",
+"assets/assets/icons/sounds/snoop.webp": "359bde2c462e91252dec6743e534d5e0",
+"assets/assets/icons/sounds/supreme_horn.jpg": "316a8f4d30e9afc90d591fc6630b638e",
+"assets/assets/icons/sounds/unreal_tournament.webp": "1186668b86a1bf9108ffce6a87f1be4b",
+"assets/assets/icons/sounds/xp.jpg": "588af854f1604538d76d3365a81c335c",
+"assets/assets/icons/sounds/you_got_mail.jpg": "d2b08137a7502202ce77841394d31f16",
+"assets/assets/references/soundlist.json": "e241854bf7337745ff4f431209766fd4",
 "assets/FontManifest.json": "1e2b2284e66a9e54db0bfe7ac4f8d5d6",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/NOTICES": "9990fac24c7889d748b4a1a90a4738ef",
@@ -28,9 +40,9 @@ const RESOURCES = {
 "favicon.png": "16daa1e75c75abade096971827289f33",
 "icons/Icon-192.png": "698704cb92d9a515459cc6ee65d70342",
 "icons/Icon-512.png": "05c3c10cf7bb027464fae82f6ba90f12",
-"index.html": "70093580e291f13eea40e66ebae9f7be",
-"/": "70093580e291f13eea40e66ebae9f7be",
-"main.dart.js": "043fca15f0c6780edf7805c352651d39",
+"index.html": "bfb475e64a3cc452e38a90e59627fb68",
+"/": "bfb475e64a3cc452e38a90e59627fb68",
+"main.dart.js": "50a894dee1372115eb379f775d41cb5d",
 "manifest.json": "57e2b04b2a9b1384bc2c10657378f0e2",
 "version.json": "99d5daa2da2f7477513c22d4eb9287e9"
 };
